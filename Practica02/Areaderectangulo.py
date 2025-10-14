3df09dc129d7f4ea1c042d94c98b899a43f92504
3duardopadilla <Jose.padilla280789@potros.itson.edu.mx>
2025-10-14 08:23:57 -0700

    Añadir calculadora de are de rectagulo

---
+++ Practica02/Areaderectangulo.py
@@ -0,0 +1,6 @@
+#Crea una calculadora para obtener el are de un rectangulo
+def calcular_area_rectangulo(b, h):
+    area = b*h
+    return area
+area = calcular_area_rectangulo (10, 5)
+print("El area de tu rectangulo es: ", area)

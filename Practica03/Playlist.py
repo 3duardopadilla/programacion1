03d3898e6ca6f3f768994f322f5f30681a9668a7
3duardopadilla <Jose.padilla280789@potros.itson.edu.mx>
2025-10-20 14:02:56 -0700

    nueva carpeta de practicas y subida de actividad de hoy

---
+++ Practica03/Playlist.py
@@ -0,0 +1,6 @@
+Playlist_musical = ["Bohemian Rhapsody", "Hotel California", "Starway to Heaven"]
+Playlist_musical.append("Do What U Want")
+Playlist_musical[1] = "Shape of You"
+Playlist_musical.insert(0, "Watermelon Sugar")
+Playlist_musical.pop(4)
+print("La lista modificada se quedo con estas canciones:", Playlist_musical)

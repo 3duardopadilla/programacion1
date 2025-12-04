2d7c3cd6a024961e87e2428236f76fe2caef06dc
3duardopadilla <Jose.padilla280789@potros.itson.edu.mx>
2025-12-04 16:52:07 -0700

    envio del resto de actividades

--- grafico/Calculadora.py
+++ grafico/Calculadora.py
@@ -2,7 +2,7 @@ import tkinter as tk
 #1. Creacion de la ventana principal
 root = tk.Tk()
 root.title("Calculadora")
-root.geometry("305x500")
+root.geometry("305x380")
 
 #   Agregar los Widgets
 botones_texto= ("C","/","x","-",
@@ -11,7 +11,28 @@ botones_texto= ("C","/","x","-",
                 "1","2","3","=",
                 "0",".","","")
 
-historico = tk.Label(root,bg="#5448F9", font="Roboto 14", width=15, bd=0)
+def click_botones(valor):
+    if valor == "=":
+        try: #Intenta hacer el codigo de dentro si falla
+            expresion = resultado.get() #Obtenemos la expresion de la caja resultado
+            evaluacion = eval(expresion)#Evaluamos la expresion
+            resultado.delete(0,tk.END)#Limpiamos caja de resultado
+            resultado.insert(tk.END,str(evaluacion))#colocamos la evualuacion en la caja de resultado
+            historico.config(text=expresion+"=")#Colocamos la expresion en el historico.
+        except Exception as e:
+            resultado.delete(0,tk.END)#Limpiamos caja de resultado
+            resultado.insert(tk.END,"Error")
+            historico.config(text="")
+    elif valor == "C":
+            resultado.delete(0,tk.END)#Limpiamos caja de resultado
+            resultado.insert(tk.END,"")
+            historico.config(text="")
+    else:
+        current_text = resultado.get() #Obtenemos la expresion
+        resultado.delete(0,tk.END) #Limpiamos la caja de resultado
+        resultado.insert(tk.END,current_text+valor)#Asignamos la nueva valor a lo que ya teniamos
+
+historico = tk.Label(root,bg="#5448F9", font="Roboto 14", width=15, bd=0, justify="right")
 
 historico.pack(pady=5, padx=10, fill="x")
 
@@ -26,7 +47,7 @@ contenedor_botones.pack(pady=5,padx=10,fill= "both")
 acumulador=0
 for row in range(5): #0
     for column in range(4): #0
-        boton = tk.Button(contenedor_botones, text=botones_texto[acumulador], bg="#130456", fg="#FFFFFF", font="Roboto 20", bd=0, width=4)
+        boton = tk.Button(contenedor_botones, text=botones_texto[acumulador], bg="#130456", fg="#FFFFFF", font="Roboto 20", bd=0, width=4, command=lambda b=botones_texto[acumulador]:click_botones(b))
 
         #Pintar los botones de colores
         if botones_texto[acumulador]=="C":
@@ -38,13 +59,13 @@ for row in range(5): #0
 
             if botones_texto[acumulador] == "+":
                 boton.config(height=3)
-                boton.grid(row=row, column=column, rowspan=2, padx=1, pady=5)
+                boton.grid(row=row, column=column, rowspan=2, padx=1, pady=1)
             elif botones_texto[acumulador] == "=":
                 boton.config(height=3, bg="#E78D16")
-                boton.grid(row=row, column=column, rowspan=2, padx=1, pady=5)
+                boton.grid(row=row, column=column, rowspan=2, padx=1, pady=1)
             elif botones_texto[acumulador] == "0":
                 boton.config(width=8)
-                boton.grid(row=row, column=column, columnspan=2, padx=1, pady=5)
+                boton.grid(row=row, column=column, columnspan=2, padx=1, pady=1)
             else:
                 boton.grid(row=row, column=column, padx=5, pady=5)
         acumulador+=1
